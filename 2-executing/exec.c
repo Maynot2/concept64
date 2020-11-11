e75d1fa575523fc8257e8ae50e8bf30459453fcd
Gregory ARTHUS <garthus@adminux.fr>
2020-11-11 09:14:43 +0000

    create(): exec.c

---
+++ 2-executing/exec.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - execve example
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *argv[] = {"/bin/ping", "google.fr", NULL};
+
+	printf("%u\n", getpid());
+	printf("Before execve\n");
+	if (execve(argv[0], argv, NULL) == -1)
+	{
+		perror("Error:");
+	}
+	printf("After execve\n");
+	return (0);
+}
